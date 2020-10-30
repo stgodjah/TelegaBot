7e93fb0eead33b17f9dea4579e062ebfc0dd0c58
godJah <43356590+stgodjah@users.noreply.github.com>
2020-10-30 14:53:37 +0200

    get updates cli

--- getUpdatesCLI.php
+++ getUpdatesCLI.php
@@ -26,9 +26,34 @@ try {
     // Create Telegram API object
     $telegram = new Longman\TelegramBot\Telegram($config['api_key'], $config['bot_username']);
     $telegram->useGetUpdatesWithoutDatabase();
-    /**
-     * Check `hook.php` for configuration code to be added here.
-     */
+
+    // Enable admin users
+    $telegram->enableAdmins($config['admins']);
+
+    // Add commands paths containing your custom commands
+    $telegram->addCommandsPaths($config['commands']['paths']);
+   
+    // Enable MySQL if required
+    $telegram->enableMySql($config['mysql']);
+   
+    // Logging (Error, Debug and Raw Updates)
+    // https://github.com/php-telegram-bot/core/blob/master/doc/01-utils.md#logging
+    //
+    // (this example requires Monolog: composer require monolog/monolog)
+    Longman\TelegramBot\TelegramLog::initialize(
+        new Monolog\Logger('telegram_bot', [
+            (new Monolog\Handler\StreamHandler($config['logging']['debug'], Monolog\Logger::DEBUG))->setFormatter(new Monolog\Formatter\LineFormatter(null, null, true)),
+            (new Monolog\Handler\StreamHandler($config['logging']['error'], Monolog\Logger::ERROR))->setFormatter(new Monolog\Formatter\LineFormatter(null, null, true)),
+        ]),
+        new Monolog\Logger('telegram_bot_updates', [
+            (new Monolog\Handler\StreamHandler($config['logging']['update'], Monolog\Logger::INFO))->setFormatter(new Monolog\Formatter\LineFormatter('%message%' . PHP_EOL)),
+        ])
+    );
+   
+    // Set custom Download and Upload paths
+    $telegram->setDownloadPath($config['paths']['download']);
+    $telegram->setUploadPath($config['paths']['upload']);
+   
 
     // Handle telegram getUpdates request
     $server_response = $telegram->handleGetUpdates();
@@ -46,8 +71,8 @@ try {
     Longman\TelegramBot\TelegramLog::error($e);
 
     // Uncomment this to output any errors (ONLY FOR DEVELOPMENT!)
-    // echo $e;
+    echo $e;
 } catch (Longman\TelegramBot\Exception\TelegramLogException $e) {
     // Uncomment this to output log initialisation errors (ONLY FOR DEVELOPMENT!)
-    // echo $e;
+    echo $e;
 }
